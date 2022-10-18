f22aae9c4d9d36d478810ecfa126a11fe1418ca6
LauraMcDev <lauramcdev089@gmail.com>
2022-10-18 11:48:22 +0200

    added messages, shortened JS code

--- index.js
+++ index.js
@@ -1,20 +1,28 @@
+//first dice
+
 let randomNumber1 = Math.floor(Math.random() * 6) + 1;
 
-let randomDiceName1 = "dice" + randomNumber1 + ".png";
+let randomSource1 = "images/dice" + randomNumber1 + ".png";
 
-let randomSource1 = "images/" + randomDiceName1;
+document.querySelectorAll("img")[0].setAttribute("src", randomSource1);
 
-let imageOne = document.querySelectorAll("img")[0];
 
-imageOne.setAttribute("src", randomSource1);
-
+//second dice
 
 let randomNumber2 = Math.floor(Math.random() * 6) + 1;
 
-let randomDiceName2 = "dice" + randomNumber2 + ".png";
+let randomSource2 = "images/dice" + randomNumber2 + ".png";
 
-let randomSource2 = "images/" + randomDiceName2;
+ document.querySelectorAll("img")[1].setAttribute("src", randomSource2);
 
-let imageTwo = document.querySelectorAll("img")[1]
 
-imageTwo.setAttribute("src", randomSource2);
+//messages
+let message = document.querySelector("h1").innerHTML;
+
+if (randomNumber1 > randomNumber2) {
+   document.querySelector("h1").innerHTML = "Player 1 wins!";
+} else if (randomNumber1 < randomNumber2) {
+   document.querySelector("h1").innerHTML = "Player 2 wins!";
+} else if (randomNumber1 === randomNumber2) {
+    document.querySelector("h1").innerHTML = "Draw!";
+}
